9adbcf27db739afab6f4d3d5059b64e5e2d826a9
enzoNzgm <enzoelie8@hotmail.fr>
2024-11-14 16:31:38 +0100

    debut des bonus

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -10,3 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
+t_list  *ft_lstnew(void *content)
+{
+    (void)content;
+    t_list *new;
+
+    new = malloc(sizeof(t_list));
+    if (!new)
+        return (NULL);
+    new->content = content;
+    new->next = NULL;
+    return (new);
+}
